1305148a3186ffc3f5c01ffbf263ed394a2ea3e6
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-09-07 15:56:53 +0530

    Completed exercise1

--- Labsheet 3/Exercise1/app/src/main/java/com/example/exercise1/SecondActivity.java
+++ Labsheet 3/Exercise1/app/src/main/java/com/example/exercise1/SecondActivity.java
@@ -2,13 +2,41 @@ package com.example.exercise1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SecondActivity extends AppCompatActivity {
+
+    EditText name,index_number,department, faculty;
+    Button ok;
+    Bundle bundle;
+    Intent check;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        ok = findViewById(R.id.okbtn);
+        name = findViewById(R.id.name);
+        index_number = findViewById(R.id.indexnumber);
+        department = findViewById(R.id.department);
+        faculty = findViewById(R.id.faculty);
+        check = new Intent(this, ThirdActivity.class);
+        bundle = new Bundle();
+        ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bundle.putString("NAME", name.getText().toString());
+                bundle.putString("INDEXNUMBER",index_number.getText().toString());
+                bundle.putString("DEPARTMENT",department.getText().toString());
+                bundle.putString("FACULTY", faculty.getText().toString());
+                check.putExtras(bundle);
+                startActivity(check);
+            }
+        });
+
     }
 }
--- Labsheet 3/Exercise1/app/src/main/java/com/example/exercise1/ThirdActivity.java
+++ Labsheet 3/Exercise1/app/src/main/java/com/example/exercise1/ThirdActivity.java
@@ -2,13 +2,31 @@ package com.example.exercise1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ThirdActivity extends AppCompatActivity {
+
+    TextView username,indexnum,dep,fac;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
+        username = findViewById(R.id.usrname);
+        indexnum = findViewById(R.id.index);
+        dep = findViewById(R.id.dept);
+        fac = findViewById(R.id.fac);
+        Intent content = getIntent();
+        Bundle get = content.getExtras();
+        String name = get.getString("NAME");
+        String indexNumber = get.getString("INDEXNUMBER");
+        String department = get.getString("DEPARTMENT");
+        String faculty = get.getString("FACULTY");
+        username.setText(name);
+        indexnum.setText(indexNumber);
+        dep.setText(department);
+        fac.setText(faculty);
     }
 }
