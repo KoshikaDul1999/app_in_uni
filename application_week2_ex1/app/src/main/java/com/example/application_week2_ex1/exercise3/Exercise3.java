3e0aab7b895dce1d1ce8a7b2d9c010b127d01b79
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-09-07 13:04:27 +0530

    create a design

---
+++ application_week2_ex1/app/src/main/java/com/example/application_week2_ex1/exercise3/Exercise3.java
@@ -0,0 +1,16 @@
+package com.example.application_week2_ex1.exercise3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.application_week2_ex1.R;
+
+public class Exercise3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercise3);
+    }
+}
