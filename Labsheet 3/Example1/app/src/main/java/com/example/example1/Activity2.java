33625fd662ad9b6b0efac04c945cd82a0b38e72c
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-09-07 13:34:59 +0530

    Update Activity2.java

--- Labsheet 3/Example1/app/src/main/java/com/example/example1/Activity2.java
+++ Labsheet 3/Example1/app/src/main/java/com/example/example1/Activity2.java
@@ -2,13 +2,26 @@ package com.example.example1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Activity2 extends AppCompatActivity {
+
+    TextView name;
+    TextView address;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+        name = findViewById(R.id.textViewname);
+        address = findViewById(R.id.textViewaddress);
+        Intent content = getIntent();
+        Bundle get = content.getExtras();
+        String name2 = get.getString("NAME");
+        String address2 = get.getString("ADDRESS");
+        name.setText(name2);
+        address.setText(address2);
     }
 }
