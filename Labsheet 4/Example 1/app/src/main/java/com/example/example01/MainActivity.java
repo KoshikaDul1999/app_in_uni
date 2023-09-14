4611c530b8e3b4c74b2978c25dc45deae40a16bb
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-09-14 14:01:59 +0530

    Completed example01

--- Labsheet 4/Example 1/app/src/main/java/com/example/example01/MainActivity.java
+++ Labsheet 4/Example 1/app/src/main/java/com/example/example01/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.example01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button go;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        go = findViewById(R.id.gobtn);
+        go.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_VIEW,(Uri.parse("http://www.gmail.com")));
+                startActivity(i);
+            }
+        });
     }
 }
