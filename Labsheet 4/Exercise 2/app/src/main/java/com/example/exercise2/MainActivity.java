c60815d67956da8f1f4473e2cdcfbbea4efe4736
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-09-19 21:49:10 +0530

    Update MainActivity.java

--- Labsheet 4/Exercise 2/app/src/main/java/com/example/exercise2/MainActivity.java
+++ Labsheet 4/Exercise 2/app/src/main/java/com/example/exercise2/MainActivity.java
@@ -1,14 +1,42 @@
 package com.example.exercise2;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView totalTextView;
+    private Button calculateButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        totalTextView = findViewById(R.id.mainText);
+        calculateButton = findViewById(R.id.button);
+
+        calculateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivityForResult(intent, 1);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK){
+            double total = data.getDoubleExtra("total", 0.0);
+            totalTextView.setText("Total: Rs." + total);
+        }
     }
 }
