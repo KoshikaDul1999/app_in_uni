bbecf80b34f9ea012445824171af8d826a8bbacd
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-09-20 13:53:17 +0530

    Update exercise4

--- Labsheet 4/Exercise 4/app/src/main/java/com/example/exercise4/MainActivity.java
+++ Labsheet 4/Exercise 4/app/src/main/java/com/example/exercise4/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.exercise4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void openEmailClient(View view) {
+        TextView emailTextView = (TextView) findViewById(R.id.email);
+        String emailAddress = emailTextView.getText().toString().split(": ")[1];
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:" + emailAddress));
+        startActivity(intent);
+    }
+
+    public void openDialer(View view) {
+        TextView phoneTextView = (TextView) findViewById(R.id.phone);
+        String phoneNumber = phoneTextView.getText().toString().split(": ")[1]; // Extract phone number
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:" + phoneNumber));
+        startActivity(intent);
+    }
+
 }
