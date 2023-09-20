980b957f54dc9dbb7d84eab2e0a81151e8e4f17a
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-09-20 13:33:28 +0530

    Update MainActivity.java

--- Labsheet 4/Exercise 3/app/src/main/java/com/example/exercise3/MainActivity.java
+++ Labsheet 4/Exercise 3/app/src/main/java/com/example/exercise3/MainActivity.java
@@ -2,13 +2,41 @@ package com.example.exercise3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView cylinderTextView;
+    private Button chooseCylinderButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cylinderTextView = findViewById(R.id.textView);
+        chooseCylinderButton = findViewById(R.id.button);
+
+        chooseCylinderButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivityForResult(intent, 1);
+            }
+        });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            String selectedCylinder = data.getStringExtra("selectedCylinder");
+            cylinderTextView.setText("Selected Cylinder: " + selectedCylinder);
+        }
+    }
+
 }
