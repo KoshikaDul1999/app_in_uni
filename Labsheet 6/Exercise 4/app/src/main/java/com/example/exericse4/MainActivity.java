13a0b15afc3c9b5d32258b038dec8887a1b58670
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-10-06 21:58:53 +0530

    Update MainActivity.java

--- Labsheet 6/Exercise 4/app/src/main/java/com/example/exericse4/MainActivity.java
+++ Labsheet 6/Exercise 4/app/src/main/java/com/example/exericse4/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.exericse4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GridView gridView = findViewById(R.id.gridView);
+
+        String[] itemNames = getResources().getStringArray(R.array.item_names);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, itemNames);
+
+        gridView.setAdapter(adapter);
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String selectedItem = itemNames[position];
+                Toast.makeText(MainActivity.this, selectedItem, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
