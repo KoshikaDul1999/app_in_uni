42b487c1c8867aea40930976c2649676888b91ad
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-10-06 17:50:01 +0530

    Update MainActivity.java

--- Labsheet 6/Exercise 2/app/src/main/java/com/example/exercise2/MainActivity.java
+++ Labsheet 6/Exercise 2/app/src/main/java/com/example/exercise2/MainActivity.java
@@ -1,14 +1,39 @@
 package com.example.exercise2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnName, btnAddress, btnPhone,  btnEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnName = findViewById(R.id.btnName);
+        btnAddress = findViewById(R.id.btnAddress);
+        btnPhone = findViewById(R.id.btnPhone);
+        btnEmail = findViewById(R.id.btnEmail);
+
+        btnName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+    }
+
+    private void displayFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragmentContainer, fragment);
+        fragmentTransaction.commit();
     }
 }
