e4b889d3c856fc8294464db9ecb6b7b529af8e2f
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-10-12 15:04:08 +0530

    add images and update MainActivity

--- Labsheet 7/Example 1/app/src/main/java/com/example/example1/MainActivity.java
+++ Labsheet 7/Example 1/app/src/main/java/com/example/example1/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.example1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity {
+    GridView simpleGrid;
+
+    int animals[] = {R.drawable.ani1, R.drawable.ani2,R.drawable.ani3, R.drawable.ani4, R.drawable.ani5, R.drawable.ani6};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        simpleGrid = findViewById(R.id.grid);
+        CustomAdapter customAdapter = new CustomAdapter(getApplicationContext(), animals);
+        simpleGrid.setAdapter(customAdapter);
     }
 }
