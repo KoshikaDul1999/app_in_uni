c065513c55d9cab0348d517960c9bdf93ce22c29
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-10-12 19:29:06 +0530

    Update list_view.xml

---
+++ Labsheet 7/Example 3/app/src/main/java/com/example/example3/ListViewItemsActivity.java
@@ -0,0 +1,14 @@
+package com.example.example3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListViewItemsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list_view_items);
+    }
+}
