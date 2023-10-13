a2b1df5c020f7b08a203e9c0385d3533eb98433f
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-10-13 07:02:08 +0530

    complete example4

---
+++ Labsheet 7/Example 4/app/src/main/java/com/example/example4/CustomAdapter.java
@@ -0,0 +1,39 @@
+package com.example.example4;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class CustomAdapter extends SimpleAdapter {
+    LayoutInflater inflater;
+    Context context;
+    ArrayList<HashMap<String, String>> arrayList;
+    public CustomAdapter(Context context, ArrayList< HashMap<String, String>> data, int resource, String[] from, int[] to) {
+        super(context, data, resource, from, to);
+        this.context = context;
+        this.arrayList = data;
+        inflater.from(context);
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        View view = super.getView(position, convertView, parent);
+        ImageView imageView = (ImageView) view.findViewById(R.id.imageView);
+        imageView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context, arrayList.get(position).get("name"), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+}
