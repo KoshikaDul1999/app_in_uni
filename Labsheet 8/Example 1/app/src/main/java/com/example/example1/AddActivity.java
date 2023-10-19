0ebbd561f546d0c92d692bf4c6d0dcd265cd2e5c
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-10-19 11:28:03 +0530

    Update AddActivity.java

--- Labsheet 8/Example 1/app/src/main/java/com/example/example1/AddActivity.java
+++ Labsheet 8/Example 1/app/src/main/java/com/example/example1/AddActivity.java
@@ -3,12 +3,37 @@ package com.example.example1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class AddActivity extends AppCompatActivity {
+    EditText name;
+    EditText dept;
+    EditText salary;
+    Button btnadd;
+    DBHandler dbHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+        name = findViewById(R.id.name);
+        dept = findViewById(R.id.dept);
+        salary = findViewById(R.id.salary);
+        btnadd = findViewById(R.id.addbtn);
+        dbHandler = new DBHandler(this);
+        btnadd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String empName = name.getText().toString();
+                String empDept = dept.getText().toString();
+                String empSalary = salary.getText().toString();
+                Employee emp = new Employee(empName, empDept, empSalary);
+                dbHandler.addEmployee(emp);
+                Toast.makeText(getApplicationContext(), "data inserted",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
