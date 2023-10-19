d29f80153cf22a96a246e5247dd2181eab213ad0
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-10-19 11:04:00 +0530

    Create DBHandler.java

---
+++ Labsheet 8/Example 1/app/src/main/java/com/example/example1/DBHandler.java
@@ -0,0 +1,30 @@
+package com.example.example1;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DBHandler extends SQLiteOpenHelper {
+
+    private static final String databaseName = "employee_details";
+    private static final int ver=1;
+
+    public DBHandler(Context context) {
+        super(context, databaseName, null, ver );
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String sql="CREATE TABLE employee(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, dep TEXT, salary TEXT);";
+        sqLiteDatabase.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        String sql = "DROP TABLE IF EXISTS employee_details;";
+        sqLiteDatabase.execSQL(sql);
+        onCreate(sqLiteDatabase);
+    }
+}
