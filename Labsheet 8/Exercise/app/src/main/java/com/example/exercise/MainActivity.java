95e52b1e6a462decace8308fa85ae1bdad939bc2
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-10-19 12:44:27 +0530

    Update MainActivity.java

--- Labsheet 8/Exercise/app/src/main/java/com/example/exercise/MainActivity.java
+++ Labsheet 8/Exercise/app/src/main/java/com/example/exercise/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.exercise;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button insert;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        insert = findViewById(R.id.insertbtn);
+
+        insert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), InsertActivity.class));
+            }
+        });
     }
 }
